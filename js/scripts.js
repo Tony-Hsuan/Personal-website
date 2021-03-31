fca0234cddb39ec59051710d2f008da71809d397
Tony-Hsuan <a5827251@gmail.com>
2021-03-31 15:38:36 +0800

    upload 1

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $(window).scroll(function (){
+        if(this.scrollY > 20){
+        $(".navbar").addClass("sticky");  
+        $(".goTop").fadeIn();
+        }
+        
+        else{
+            $(".navbar").removeClass("sticky");
+            $(".goTop").fadeOut();
+
+        }
+    });
+
+
+
+    $(".goTop").click(function(){
+        scroll(0,0)
+    })
+
+
+    $(".menu-toggler").click(function(){
+        $(this).toggleClass("active");
+        $(".navbar-menu").toggleClass("active");
+    });
+
+
+    $("roadtrip").option({
+        'resizeDuration': 200,
+        'wrapAround': true
+    });
+      //light-box燈罩
+
+});
